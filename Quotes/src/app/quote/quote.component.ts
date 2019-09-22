957e0c11c34e06b973111036fb5519f126cfe8e9
AndyOmondi17 <andyomondi17@gmail.com>
2019-09-22 19:56:42 +0300

    create instances of the class Quote

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes:Quote[]=[
+    new Quote(1,"A journey of a thousand miles begins with one step","Chinese man","Andy Omondi",new Date(2019,0,16)),
+    new Quote(2,"I have no special talent. I am only passionately curious.","Albert Einstein","Andy Omondi",new Date(2019,0,16)),
+    new Quote(3,"Stay hungry, stay foolish.","Steve Jobs","Andy Omondi",new Date(2019,0,16)),
+    new Quote(4,"The successful warrior is the average man, with laser-like focus","Bruce Lee","Andy Omondi",new Date(2019,0,16)),
+    new Quote(5,"The successful warrior is the average man, with laser-like focus","Theodore Rosevelte","Andy Omondi",new Date(2019,0,16)),
+
+  ];
+
 
   constructor() { }
 
