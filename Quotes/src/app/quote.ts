8f089b1a8317c20cdc4d469b548fdd22bde997d4
AndyOmondi17 <andyomondi17@gmail.com>
2019-09-22 20:35:27 +0300

    create form

--- Quotes/src/app/quote.ts
+++ Quotes/src/app/quote.ts
@@ -3,7 +3,7 @@ export class Quote {
     public downvotes:number;
     public showDetails:boolean;
     
-    constructor(public id:number,public name:string,public author:string,public submittedBy:string, public dateCreation:Date ){
+    constructor(public id:number,public Name:string,public author:string,public submittedBy:string, public dateCreation:Date ){
         this.upvotes=0;
         this.downvotes=0;
         this.showDetails=false;
